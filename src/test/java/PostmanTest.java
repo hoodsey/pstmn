b57ff9d0dbd19a2c458883f1fb0c989ae586ad04
Nasya Kabakova <kabakova.nas@gmail.com>
2023-10-15 19:25:49 +0300

    Fail commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class PostmanTest {
+
+    @Test
+    void ShoildPost(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Not HI"))
+        ;
+    }
+
+}
