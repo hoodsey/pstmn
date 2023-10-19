23646d8111aa3e83398b874c9cb27f6021313c45
Nasya Kabakova <kabakova.nas@gmail.com>
2023-10-19 11:49:50 +0300

    success test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -14,7 +14,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("HI"))
+                .body("data", equalTo("Hi"))
         ;
     }
 
